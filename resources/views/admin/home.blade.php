41e5b83409991342ded34aac2c731048c5244c78
Coderman <coderman1980@gmail.com>
2023-08-10 16:47:37 -0500

    AdminLTE

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Administrador')
+
+@section('content_header')
+    <h1>Panel Administrativo</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel administrativo JStock.</p>
+@stop
+
+@section('css')
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
